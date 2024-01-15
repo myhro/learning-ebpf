dc3587dc2c8523dfa8ec9984b7b029b42d6df7a9
Tiago Ilieve <tiago.myhro@gmail.com>
2024-01-14 22:27:23 -0300

    exercise-03: attach to 'sys_enter' raw tracepoint

--- chapter-02/exercise-03-hello-map.py
+++ chapter-02/exercise-03-hello-map.py
@@ -22,8 +22,7 @@ int hello(void *ctx) {
 """
 
 b = BPF(text=program)
-syscall = b.get_syscall_fnname("execve")
-b.attach_kprobe(event=syscall, fn_name="hello")
+b.attach_raw_tracepoint(tp="sys_enter", fn_name="hello")
 
 # Attach to a tracepoint that gets hit for all syscalls
 # b.attach_raw_tracepoint(tp="sys_enter", fn_name="hello")
