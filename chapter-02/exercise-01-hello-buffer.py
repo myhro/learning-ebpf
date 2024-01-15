ed76841143ea033166e72da96689f28fac4e2913
Tiago Ilieve <tiago.myhro@gmail.com>
2024-01-14 22:20:57 -0300

    exercise-01: add odd/even messages

--- chapter-02/exercise-01-hello-buffer.py
+++ chapter-02/exercise-01-hello-buffer.py
@@ -2,6 +2,8 @@
 from bcc import BPF
 
 program = r"""
+#include <linux/string.h>
+
 BPF_PERF_OUTPUT(output);
 
 struct data_t {
@@ -13,10 +15,15 @@ struct data_t {
 
 int hello(void *ctx) {
    struct data_t data = {};
-   char message[12] = "Hello World";
+   char message[12];
 
    data.pid = bpf_get_current_pid_tgid() >> 32;
    data.uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+
+   strcpy(message, "odd");
+   if (data.pid % 2 == 0) {
+       strcpy(message, "even");
+   }
 
    bpf_get_current_comm(&data.command, sizeof(data.command));
    bpf_probe_read_kernel(&data.message, sizeof(data.message), message);
